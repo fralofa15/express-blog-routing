d972c19e90714a9b10e13ab73cb818cbc7179bae
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-05-27 17:36:12 +0200

    - Creata cartella img - Aggiunto array di oggetti in data.js

---
+++ data/data.js
@@ -0,0 +1,34 @@
+const posts = [
+    {
+        titolo: "Tyrannosaurus Rex",
+        contenuto: "Uno dei dinosauri carnivori più famosi, vissuto nel Cretaceo superiore. Era un predatore potente, con mascelle molto robuste e denti affilati.",
+        immagine: "/img/trex.jpg",
+        tags: ["dinosauri", "carnivori", "cretaceo"]
+    },
+    {
+        titolo: "Triceratops",
+        contenuto: "Dinosauro erbivoro riconoscibile per le tre corna sul cranio e il grande collare osseo. Viveva in Nord America durante il Cretaceo superiore.",
+        immagine: "/img/triceratopo.jpg",
+        tags: ["dinosauri", "erbivori", "cretaceo"]
+    },
+    {
+        titolo: "Velociraptor",
+        contenuto: "Piccolo dinosauro carnivoro, agile e veloce. Nonostante la fama cinematografica, era molto più piccolo rispetto a come viene spesso rappresentato.",
+        immagine: "/img/velociraptor.jpg",
+        tags: ["dinosauri", "carnivori", "raptor"]
+    },
+    {
+        titolo: "Brachiosaurus",
+        contenuto: "Grande dinosauro erbivoro dal collo molto lungo. Si nutriva probabilmente di vegetazione alta e aveva le zampe anteriori più lunghe di quelle posteriori.",
+        immagine: "/img/brachiosauro.jpg",
+        tags: ["dinosauri", "erbivori", "giurassico"]
+    },
+    {
+        titolo: "Stegosaurus",
+        contenuto: "Dinosauro erbivoro famoso per le grandi placche ossee lungo il dorso e le spine sulla coda, usate probabilmente come difesa.",
+        immagine: "/img/stegosauro.jpg",
+        tags: ["dinosauri", "erbivori", "giurassico"]
+    }
+];
+
+export default posts;
